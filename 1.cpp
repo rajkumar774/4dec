292bbc4b9c738f159245a4b7ce3bdf6dd74b9005
Rajkumar <nsrajkumar552@gmail.com>
2024-12-04 21:10:40 +0530

    Committed

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	a=10;
+	b=20;//Arithmetic Operators
+	cout<<"The arithmetic operator"<<a%b;
+	cout<<"\nThe arithmetic operator"<<a/b;
+	cout<<"\nThe arithmetic operator"<<a*b;
+	cout<<"\nThe arithmetic operator"<<a+b;
+	cout<<"\nThe arithmetic operator"<<a-b;
+a++;
+	cout<<"\nThe arithmetic operator"<<a;
+	return 0;
+	
+}
